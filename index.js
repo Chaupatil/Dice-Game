5362dbc910ec4ed5271d7942df8d0da5be0ad2e0
Chaupatil <patilchau@gmail.com>
2024-08-07 22:57:33 +0530

    updating files

--- index.js
+++ index.js
@@ -1,29 +1,23 @@
+function rollDice() {
+  var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+  var randomImageSource1 = "images/dice" + randomNumber1 + ".png";
+  document.querySelector(".img1").setAttribute("src", randomImageSource1);
 
-var randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
+  var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+  var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+  document.querySelector(".img2").setAttribute("src", randomImageSource2);
 
-var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
-
-var randomImageSource = "images/" + randomDiceImage; //images/dice1.png - images/dice6.png
-
-var image1 = document.querySelectorAll("img")[0];
-
-image1.setAttribute("src", randomImageSource);
-
-
-var randomNumber2 = Math.floor(Math.random() * 6) + 1;
-
-var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
-
-document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
-
-
-//If player 1 wins
-if (randomNumber1 > randomNumber2) {
-  document.querySelector("h1").innerHTML = "🚩 Play 1 Wins!";
+  var header = document.querySelector("#message");
+  if (randomNumber1 > randomNumber2) {
+    header.innerHTML = "🚩 Player 1 Wins! Roll Again!";
+  } else if (randomNumber2 > randomNumber1) {
+    header.innerHTML = "Player 2 Wins! 🚩 Roll Again!";
+  } else {
+    header.innerHTML = "Draw! Roll Again!";
+  }
 }
-else if (randomNumber2 > randomNumber1) {
-  document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
-}
-else {
-  document.querySelector("h1").innerHTML = "Draw!";
-}
+
+document.addEventListener("DOMContentLoaded", function() {
+  var header = document.querySelector("#message");
+  header.innerHTML = "Roll the Dice!";
+});
